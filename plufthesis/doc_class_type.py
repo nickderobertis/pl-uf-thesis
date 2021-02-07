46696e080f5441009139b044291786dc46b28af9
TODO <todo-actions[bot]@users.noreply.github.com>
2021-02-07 19:02:09 +0000

    Collect TODO comments

--- plufthesis/doc_class_type.py
+++ plufthesis/doc_class_type.py
@@ -9,7 +9,7 @@ UF_THESIS_DISSERTATION_CLS_TEXT = UF_THESIS_DISSERTATION_CLS_FILE.read_text()
 
 class_type = DocumentClassType('uf-thesis-dissertation', UF_THESIS_DISSERTATION_CLS_TEXT)
 
-# TODO: replace this once pyexlatex has a more convenient way of adding custom class types
+# TODO [$602039315315e00008a1abd9]: replace this once pyexlatex has a more convenient way of adding custom class types
 def register_doc_type():
     from pyexlatex.models.control.documentclass.classtypes.custom import CUSTOM_CLASS_TYPES
     CUSTOM_CLASS_TYPES[class_type.name] = class_type
